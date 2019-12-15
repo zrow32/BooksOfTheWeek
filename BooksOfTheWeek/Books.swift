4fed05441bbedef2ae6640afcfd8305ce5c6821a
zrow32 <z.rows32@gmail.com>
2019-12-15 18:08:04 +0300

    Parse JSON in MainVC

---
+++ BooksOfTheWeek/Books.swift
@@ -0,0 +1,41 @@
+//
+//  Books.swift
+//  BooksOfTheWeek
+//
+//  Created by Vladimir Zhirov on 12/12/19.
+//  Copyright © 2019 Vladimir Zhirov. All rights reserved.
+//
+
+import Foundation
+
+
+struct  BooksResponse: Decodable {
+
+    var status: String?
+    var copyright: String?
+    var num_results: Int?
+    var last_modified: String?
+    var results: Results
+}
+
+
+struct Results: Decodable {
+      var books: [Book]
+    
+    // НЕ используется
+    var list_name:   String?
+    var list_name_encoded: String?
+    var bestsellers_date: String?
+}
+
+    
+struct Book: Decodable {
+    var author: String?
+    var title: String?
+    var description: String?
+    var book_image: String?
+}
+
+
+
+
